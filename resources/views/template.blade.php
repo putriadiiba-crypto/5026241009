744f1aaae59c2faca1091e9ca91a944c07284015
Adiiba Putri Hanifah <putriadiiba@gmail.com>
2026-05-25 11:54:56 +0700

    PERTEMUAN 12

---
+++ resources/views/template.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Dimas Ananda D - 5026241113</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="mt-4 p-5 bg-primary text-white rounded">
+            <h3>Adiiba Putri H. - 5026241009</h3>
+            <h6>@yield('title')</h6>
+        </div>
+        <!-- Grey with black text -->
+        <nav class="navbar navbar-expand-sm bg-light navbar-light">
+            <div class="container-fluid">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="/pegawai">Pegawai</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">PR 1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">PR 2</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">PR 3</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">EAS</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="container">
+            @yield('konten')
+        </div>
+    </div>
+
+</body>
+
+</html>
